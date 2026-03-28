b996236cb4c4f727aefac174e185ecbb6287ca25
Noam Knispel <noam.knispel@melio.com>
2026-03-27 22:22:17 -0400

    change default

--- apps/microcontroller/code.py
+++ apps/microcontroller/code.py
@@ -188,8 +188,8 @@ def set_status(msg):
 
 
 def clear_row(row):
-    bullets[row].fill = DEFAULT_LINE_COLOR
-    line_labels[row].text = " "
+    bullets[row].fill = 0x000000  # Black (invisible on black background)
+    line_labels[row].text = ""
     dest_labels[row].text = ""
     eta_labels[row].text = ""
 
